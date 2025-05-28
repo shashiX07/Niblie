c2579c3c7721cd43b766b68823aa7c4e1a72fe4f
shashix07 <shashi007.iitkgp@gmail.com>
2025-05-29 00:12:21 +0530

    settings pafe fix

--- modules/modal.js
+++ modules/modal.js
@@ -364,7 +364,7 @@ const ModalUI = {
 
     settingsButton.title = "Settings";
     settingsButton.addEventListener("click", () => {
-      const settingURL = "https://github.com/shashix07";
+      const settingURL = "https://shashix.github.io/Niblie/";
       window.location.href = settingURL;
     })
 
